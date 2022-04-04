a0d5b7f9227a2fdd57b1939c6b84963973dc99fb
tomdavid <tomdavid09022003@gmail.com>
2022-04-04 21:49:06 +0200

    Deplacement
    Deplacement et resolution de bug

--- Projet-Unity/Assets/Script/PlayerMouvement.cs
+++ Projet-Unity/Assets/Script/PlayerMouvement.cs
@@ -5,27 +5,38 @@ using UnityEngine;
 public class PlayerMouvement : MonoBehaviour
 {
     [Header("Mouvement")]
-
-    public float moveSpeed;
+    private float moveSpeed;
+    public float walkSpeed;
+    public float sprintSpeed;
 
     public float groundDrag;
 
+    [Header("Saut")]
     public float jumpForce;
     public float jumpCooldown;
     public float airMultiplier;
     bool readyToJump;
 
+    [Header("S'accroupir")]
+    public float crouchSpeed;
+    public float crouchYScale;
+    private float startYScale;
+
     [Header("Keybinds")]
     public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public KeyCode crouchKey = KeyCode.LeftControl;
 
-
-
-
-
-    [Header("Ground")]
+    [Header("Ground Check")]
     public float playerHeight;
     public LayerMask whatIsGround;
     bool grounded;
+
+    [Header("Verif Angle Pente")]
+    public float maxPenteAngle;
+    private RaycastHit penteHit;
+    private bool sortiePente;
+    
 
     public Transform orientation;
 
@@ -36,22 +47,33 @@ public class PlayerMouvement : MonoBehaviour
 
     Rigidbody rb;
 
-    
-   private void Start()
+    public MovementState state;
+    public enum MovementState
+    {
+        walking,
+        sprinting,
+        crouching,
+        air
+    }
+
+    private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
 
         readyToJump = true;
+
+        startYScale = transform.localScale.y;
     }
 
     private void Update()
     {
         // ground check
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, whatIsGround);
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
 
         MyInput();
         SpeedControl();
+        StateHandler();
 
         // handle drag
         if (grounded)
@@ -70,6 +92,7 @@ public class PlayerMouvement : MonoBehaviour
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
+        // Si le joueur press espace et est au sol et est pret a sauter alors il saute avec un cooldown definie
         if(Input.GetKey(jumpKey) && readyToJump && grounded)
         {
             readyToJump = false;
@@ -78,32 +101,105 @@ public class PlayerMouvement : MonoBehaviour
 
             Invoke(nameof(ResetJump), jumpCooldown);
         }
+
+        // Debut de l'accroupie qui nous retrecie
+        if (Input.GetKeyDown(crouchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+            rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+        }
+
+        // Arret de l'accroupie et on reprend notre forme du debut
+        if (Input.GetKeyUp(crouchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+        }
+    }
+
+    private void StateHandler()
+    {
+        // Mode - Accroupie = reduction de vitesse et state
+        if (Input.GetKey(crouchKey))
+        {
+            state = MovementState.crouching;
+            moveSpeed = crouchSpeed;
+        }
+
+        // Mode - sprint = Augmentation de vitesse
+        else if(grounded && Input.GetKey(sprintKey))
+        {
+            state = MovementState.sprinting;
+            moveSpeed = sprintSpeed;
+        }
+
+        // Mode - Marche = Vitesse de marche normal si on appuie sur aucune touche hors deplacement
+        else if (grounded)
+        {
+            state = MovementState.walking;
+            moveSpeed = walkSpeed;
+        }
+
+        // Mode - Air = quand on ne touche pas le sol(ne peut sauter)
+        else
+        {
+            state = MovementState.air;
+        }
     }
 
     private void MovePlayer()
     {
+        // calcule des mouvent et de la direction(universelle code)
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        if(grounded)
+        // Sur une pente (resolution bug pour monter une pente trop incliner et rendu realiste)
+        if (SurPente() && !sortiePente)
+        {
+            rb.AddForce(GetPenteMoveDirection() * moveSpeed * 20f, ForceMode.Force);
+
+            if (rb.velocity.y > 0)
+                rb.AddForce(Vector3.down * 80f, ForceMode.Force);
+        }
+
+        // touche le sol
+        else if(grounded)
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
 
+        // est dans les air
         else if(!grounded)
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+
+        // Suppretion de la gravité pour les pentes
+        rb.useGravity = !SurPente();
     }
 
     private void SpeedControl()
     {
-        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-
-        if(flatVel.magnitude > moveSpeed)
+        // ralentissement de vitesse dans les pente
+        if (SurPente() && !sortiePente)
         {
-            Vector3 limitedVel = flatVel.normalized * moveSpeed;
-            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+            if (rb.velocity.magnitude > moveSpeed)
+                rb.velocity = rb.velocity.normalized * moveSpeed;
+        }
+
+        // limitation de vitesse (acceleration infinie et exponentiel)
+        else
+        {
+            Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+            
+            if (flatVel.magnitude > moveSpeed)
+            {
+                Vector3 limitedVel = flatVel.normalized * moveSpeed;
+                rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+            }
         }
     }
 
     private void Jump()
     {
+        sortiePente = true;
+
+        // reset y vitesse pour des saut equivalent
         rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
@@ -111,5 +207,23 @@ public class PlayerMouvement : MonoBehaviour
     private void ResetJump()
     {
         readyToJump = true;
+
+        sortiePente = false;
+    }
+
+    private bool SurPente()
+    {
+        if(Physics.Raycast(transform.position, Vector3.down, out penteHit, playerHeight * 0.5f + 0.3f))
+        {
+            float angle = Vector3.Angle(Vector3.up, penteHit.normal);
+            return angle < maxPenteAngle && angle != 0;
+        }
+
+        return false;
+    }
+
+    private Vector3 GetPenteMoveDirection()
+    {
+        return Vector3.ProjectOnPlane(moveDirection, penteHit.normal).normalized;
     }
 }
